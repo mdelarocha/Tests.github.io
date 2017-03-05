9f877c4853f7a692adb4a531aa9653c17f9c335e
jvl2020 <jvl150030@utdallas.edu>
2017-03-04 18:14:02 -0600

    Update input.js

--- input.js
+++ input.js
@@ -1,1 +1,8 @@
-
+function getSearch() {
+    var x = document.getElementById("frm1");
+    var text = "";
+    for (var i = 0; i < x.length ;i++) {
+        text += x.elements[i].value + "<br>";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
